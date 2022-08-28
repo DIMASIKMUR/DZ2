2a62afa9a71ae8f82a8568a569885625033953b2
DIMASIK <mdsy98@mail.ru>
2022-08-28 20:37:18 +0300

    FINAL

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.WriteLine("Задача 15 ");
 
-Console.Write("Введи цифру, обозначающую день недели: ");
+Console.Write("Введите цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
 void CheckingTheDayOfTheWeek (int dayNumber) {
   if (dayNumber == 6 || dayNumber == 7) {
-  Console.WriteLine("(этот день выходной) -> да");
+  Console.WriteLine(" Выходной , Ура ");
   }
  
-  else Console.WriteLine("(этот день не выходной) -> нет");
+  else Console.WriteLine("Работай бедолага -это не выходной");
 }
 
 CheckingTheDayOfTheWeek(dayNumber);
